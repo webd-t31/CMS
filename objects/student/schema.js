4416f74f31fc7f4a910a037c331b5e5d77ced12d
webd-t31 <toshitmall2000@gmail.com>
2022-05-15 16:23:00 +0530

    deleted obj

--- objects/student/schema.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "firstName": {type: "string", req: true},
-    "lastName": {type: "string", req: false},
-    "email": {type: "string", req: true},
-    "usn": {type: "string", req: true}
-}
